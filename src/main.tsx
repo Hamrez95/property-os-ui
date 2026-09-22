a62a4eaa5cd2737197c061d425ab1301203c160c
Hamidreza Pakpour <136631329+Hamrez95@users.noreply.github.com>
2026-09-22 18:36:03 +0330

    design: enable extended admin styling

--- src/main.tsx
+++ src/main.tsx
@@ -9,6 +9,7 @@ import './design-system/property-spaces.css'
 import './design-system/building-trust.css'
 import './design-system/marketplace-account.css'
 import './design-system/inspector.css'
+import './design-system/admin-extended.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode><App /></React.StrictMode>
