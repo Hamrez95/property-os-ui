47e5c9639ef90a37048c1efd21662543e20c60e2
Hamidreza Pakpour <136631329+Hamrez95@users.noreply.github.com>
2026-09-22 18:28:05 +0330

    design: enable building and trust styling

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import './styles.css'
 import './design-system/foundation.css'
 import './design-system/reference-screens.css'
 import './design-system/property-spaces.css'
+import './design-system/building-trust.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode><App /></React.StrictMode>
