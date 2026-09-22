56f9a9c480a5f8b51fbab089657f9ace287663ac
Hamidreza Pakpour <136631329+Hamrez95@users.noreply.github.com>
2026-09-22 18:34:32 +0330

    design: enable inspector visual polish

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,7 @@ import './design-system/reference-screens.css'
 import './design-system/property-spaces.css'
 import './design-system/building-trust.css'
 import './design-system/marketplace-account.css'
+import './design-system/inspector.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode><App /></React.StrictMode>
